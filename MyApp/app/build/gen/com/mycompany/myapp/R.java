1e0c5737a3a880f41b64a6f7d8030123d84570b0
jeffsil <jeff_s_il@yahoo.com>
2018-11-25 00:57:10 -0600

    flash

--- MyApp/app/build/gen/com/mycompany/myapp/R.java
+++ MyApp/app/build/gen/com/mycompany/myapp/R.java
@@ -11,16 +11,50 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_info_24dp=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int ic_settings_24dp=0x7f020002;
+        public static final int power_symbol=0x7f020003;
+    }
+    public static final class id {
+        public static final int action_about=0x7f080005;
+        public static final int flashlightButton=0x7f080003;
+        public static final int i_love_it=0x7f080000;
+        public static final int maybe_later=0x7f080002;
+        public static final int needs_work=0x7f080001;
+        public static final int settings=0x7f080004;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int about=0x7f030000;
+        public static final int main=0x7f030001;
+    }
+    public static final class menu {
+        public static final int options_menu=0x7f070000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
+        public static final int about_intro_text=0x7f05000f;
+        public static final int about_meh_btn_text=0x7f05000d;
+        public static final int about_neg_button_text=0x7f05000c;
+        public static final int about_pos_btn_text=0x7f05000b;
+        public static final int about_rate_heading=0x7f05000e;
+        public static final int about_rate_text=0x7f05000a;
+        public static final int action_about_title=0x7f050009;
+        public static final int app_name=0x7f050000;
+        public static final int pref_long_press=0x7f050007;
+        public static final int pref_long_press_summary=0x7f050003;
+        /**  Strings related to Settings 
+         */
+        public static final int pref_long_press_title=0x7f050002;
+        public static final int pref_white=0x7f050008;
+        public static final int title_activity_settings=0x7f050001;
+        public static final int title_menu_settings=0x7f050006;
+        public static final int white_summary=0x7f050005;
+        public static final int white_title=0x7f050004;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
+        public static final int AppTheme=0x7f060000;
+    }
+    public static final class xml {
+        public static final int preferences=0x7f040000;
     }
 }
